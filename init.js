ec1b5692aad32f7ad57bd08b3ec49863eb774095
Roberto Oyarzun <royarzun@integer.cl>
2024-08-12 23:19:15 -0400

    Primera carga
    Inicialización

---
+++ init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBoXlzJymk3bcDZVSWI6IAEWR59BSDqq5E",
+    authDomain: "dbwatcher-39d27.firebaseapp.com",
+    projectId: "dbwatcher-39d27",
+    storageBucket: "dbwatcher-39d27.appspot.com",
+    messagingSenderId: "540704749050",
+    appId: "1:540704749050:web:d5c13fb928e562b295a043"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
